46763eb4a289956cda2f67b3035e97dab6530d34
Sora Tonami <ms0503@outlook.com>
2025-03-16 19:03:38 +0900

    feature: getemoji: change tls backend openssl -> rustls

--- pkgs/getemoji/default.nix
+++ pkgs/getemoji/default.nix
@@ -1,16 +1,10 @@
-{
-  rustPlatform,
-  openssl,
-  pkg-config,
-}:
+{ rustPlatform }:
 let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 in
 rustPlatform.buildRustPackage {
-  buildInputs = [ openssl ];
   inherit (cargoToml.package) version;
   cargoLock.lockFile = ./Cargo.lock;
-  nativeBuildInputs = [ pkg-config ];
   pname = cargoToml.package.name;
   src = ./.;
 }
