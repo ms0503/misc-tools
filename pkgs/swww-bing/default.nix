17b3980feaa119508c189486d34df547eba6d199
Sora Tonami <ms0503@outlook.com>
2025-03-16 19:05:14 +0900

    feature: swww-bing: change tls backend openssl -> rustls

--- pkgs/swww-bing/default.nix
+++ pkgs/swww-bing/default.nix
@@ -1,20 +1,10 @@
-{
-  openssl,
-  pkg-config,
-  rustPlatform,
-}:
+{ rustPlatform }:
 let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 in
 rustPlatform.buildRustPackage {
-  buildInputs = [
-    openssl
-  ];
   inherit (cargoToml.package) version;
   cargoLock.lockFile = ./Cargo.lock;
-  nativeBuildInputs = [
-    pkg-config
-  ];
   pname = cargoToml.package.name;
   src = ./.;
 }
