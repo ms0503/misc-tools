227c34dc294fcbed500ee195218b47d78ba464b1
Sora Tonami <ms0503@outlook.com>
2025-04-07 02:04:19 +0900

    chore: use mold

--- pkgs/getemoji/default.nix
+++ pkgs/getemoji/default.nix
@@ -1,10 +1,14 @@
-{ rustPlatform }:
+{ mold, rustPlatform }:
 let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 in
 rustPlatform.buildRustPackage {
   inherit (cargoToml.package) version;
+  RUSTFLAGS = "-Clink-arg=-fuse-ld=mold";
   cargoLock.lockFile = ./Cargo.lock;
+  nativeBuildInputs = [
+    mold
+  ];
   pname = cargoToml.package.name;
   src = ./.;
 }
